5aaf5d5e20d8ea7eccde8aa25ac8d48cee16af9c
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-06-08 14:56:01 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # MonteCarloPathTracing
 
 > 2D Path tracer with Python 3 and Monte Carlo method
+
+## Curso
+
+Análisis de Algoritmos
+
+## Instalación y uso
+
+**Se requiere tener instalado pipenv**
+
+```bash
+$ pipenv install # Instala los paquetes del proyecto
+
+$ pipenv shell # Correr ambiente virtual
+```
---
+++ main.py
@@ -0,0 +1,16 @@
+import tkinter
+import numpy as np
+from tkinter import *
+
+
+def main():
+    # Crear main window
+    root_window = tkinter.Tk()
+    root_window.title('Proyecto #2 Analisis de Algoritmos - 2D Path Tracing')
+    arr = np.array([2, 3, 4])
+    print(arr)
+    root_window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
