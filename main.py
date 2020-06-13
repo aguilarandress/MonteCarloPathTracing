d8ecf4ce10200a1669d43c9903d17a0cff153d5e
andres.aguilar10@hotmail.com <andres.aguilar10@hotmail.com>
2020-06-13 14:58:46 -0600

    Ventana inicial

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-import tkinter
 import numpy as np
+import pygame
+from PIL import Image
 from scene_elements.Point import Point
 from scene_elements.Segment import Segment
-from tkinter import *
 
 
 def main():
     """Funcion principal de la aplicacion
     """
-    # Crear main window
-    root_window = tkinter.Tk()
-    root_window.title('Proyecto #2 Analisis de Algoritmos - 2D Path Tracing')
-    arr = np.array([2, 3, 4])
-    print(arr)
-    root_window.mainloop()
+    # Crear ventana
+    HEIGHT, WIDTH = 500, 500
+    border = 50
+    pygame.init()
+    window = pygame.display.set_mode((500, 500))
+    screen = pygame.display.set_mode(
+        (WIDTH + (2 * border), HEIGHT + (2 * border)))
+    pygame.display.set_caption("Path Tracer")
+    clock = pygame.time.Clock()
+    # Cargar imagen
+    img_file = Image.open("assets/fondoW.png")
+    img_ref = np.array(img_file)
+    # Desplegar imagen
+    surface = pygame.surfarray.make_surface(img_ref)
+    screen.blit(surface, (border, border))
+    # Main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        pygame.display.update()
 
 
 if __name__ == "__main__":
